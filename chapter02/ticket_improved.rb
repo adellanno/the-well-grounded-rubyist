4fbb89243210e84dfe01ed960956795652ee2b52
adellanno <anthony@dellanno.co.uk>
2015-08-27 14:50:13 +0000

    Ticket improved using string interpolation

---
+++ chapter02/ticket_improved.rb
@@ -0,0 +1,29 @@
+ticket = Object.new
+
+def ticket.date
+  "01/02/03"
+end
+
+def ticket.venue
+  "Town Hall"
+end
+
+def ticket.event
+  "Author's reading"
+end
+
+def ticket.performer
+  "Mark Twain"
+end
+
+def ticket.seat
+  "Second Balcony, row J, seat 12"
+end
+
+def ticket.price
+  5.50
+end
+
+print "This ticket is for: #{ticket.event}, at #{ticket.venue}, on #{ticket.date}." +
+"The performer is #{ticket.performer}." +
+"The seat is #{ticket.seat}, and it costs £#{"%.2f." % ticket.price}"
